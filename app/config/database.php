de447e231e83f6c58fe9889698406ad9037d2376
luisfer <luismec90>
2015-12-18 13:15:28 -0500

    Initial commit

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'contactbook',
+			'username'  => 'contactbook',
+			'password'  => 'contactbook..2015!',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
