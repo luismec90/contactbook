6903de5351f97a51622f69f91299d64375774749
luisfer <luismec90>
2015-12-18 11:33:14 -0500

    Initial commit

---
+++ app/config/packages/artdarek/oauth-4-laravel/config.php
@@ -0,0 +1,37 @@
+<?php 
+
+return array( 
+	
+	/*
+	|--------------------------------------------------------------------------
+	| oAuth Config
+	|--------------------------------------------------------------------------
+	*/
+
+	/**
+	 * Storage
+	 */
+	'storage' => 'Session', 
+
+	/**
+	 * Consumers
+	 */
+	'consumers' => array(
+
+		/**
+		 * Facebook
+		 */
+        'Facebook' => array(
+            'client_id'     => '',
+            'client_secret' => '',
+            'scope'         => array(),
+        ),
+		'Github' => [
+			'client_id' => 'f8f2f375c4b46951d922',
+			'client_secret' => '9541a74190461ab1a1d68c9c1a375197f1520ef8',
+			'redirect' => 'http://contactbook2.dev/auth/github',
+		],
+
+	)
+
+);
---
+++ app/controllers/LoginController.php
@@ -0,0 +1,51 @@
+<?php
+
+class LoginController extends BaseController
+{
+
+    public function index()
+    {
+        return View::make('login');
+    }
+
+    public function loginWithGithub()
+    {
+        dd(":a");
+        // get data from input
+        $code = Input::get('code');
+
+        // get fb service
+        $gh = OAuth::consumer('Github');
+
+        // check if code is valid
+
+        // if code is provided get user data and sign in
+        if (!empty($code)) {
+
+            // This was a callback request from facebook, get the token
+            $gh->requestAccessToken($code);
+
+            // Send a request with it
+
+            $result = json_decode($gh->request('user/emails'), true);
+            echo 'The first email on your github account is ' . $result[0];
+
+            //Var_dump
+            //display whole array().
+            dd($result);
+
+        } // if not ask for permission first
+        else {
+            // get fb authorization
+            $url = $gh->getAuthorizationUri();
+
+            // return to facebook login url
+            return Redirect::to((string)$url);
+        }
+    }
+
+    public function githubResponse()
+    {
+
+    }
+}
