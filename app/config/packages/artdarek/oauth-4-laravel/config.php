7eaa43053c518b20d873c2f77cf8f828a12a420d
luisfer <luismec90>
2015-12-18 12:46:42 -0500

    Initial commit

--- app/config/packages/artdarek/oauth-4-laravel/config.php
+++ app/config/packages/artdarek/oauth-4-laravel/config.php
@@ -26,7 +26,7 @@ return array(
             'client_secret' => '',
             'scope'         => array(),
         ),
-		'Github' => [
+		'github' => [
 			'client_id' => 'f8f2f375c4b46951d922',
 			'client_secret' => '9541a74190461ab1a1d68c9c1a375197f1520ef8',
 			'redirect' => 'http://contactbook.luisfer.co/auth/github',
